567b9fb14a9321a7620774f012d478c6928e5d12
azizbekqiyomov5555-arch <azizbekqiyomov5555@gmail.com>
2026-05-20 08:57:31 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,7 +29,7 @@ from telegram.ext import (
 )
 
 # ─── KONFIGURATSIYA ────────────────────────────────────────
-BOT_TOKEN  = os.environ.get("BOT_TOKEN")  or "8723400610:AAHiYOyPlTNC8msWD_IZabGSWgB3cPDmV8Q"
+BOT_TOKEN  = os.environ.get("BOT_TOKEN")  or "8723400610:AAE5bz5FPDLuEccQ5xzOOQePSrOybiXY3NE"
 ADMIN_ID   = int(os.environ.get("ADMIN_ID") or "8537782289")
 
 DATABASE_URL      = os.environ.get("DATABASE_URL") or ""
